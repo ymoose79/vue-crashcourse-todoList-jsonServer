484b6cc80bbcf501900e651e735ccffdb4cef50f
ymoose79 <justin.r.stock@icloud.com>
2022-03-29 15:06:35 -0400

    point of return

---
+++ src/components/Task.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- passing in task but adding reminder class -->
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
+    <h3>
+      {{ task.text }}
+      <!-- @dbl emit is accomplishes same goal as task.id (bubbling up data) but with 
+1) different syntax and 
+2) no need to include it in methods: below -->
+      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  methods: {
+    onDelete(id) {
+      this.$emit("delete-task", id);
+    },
+  },
+};
+</script>
+
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- remove :key to see warning -->
+  <div :key="task.id" v-for="task in tasks">
+      <Task 
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)" :task="task"/>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+//   added to remove warning from passing data up the tree
+  emits: ['delete-task', 'toggle-reminder'],
+};
+</script>
